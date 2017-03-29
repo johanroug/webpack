fa6215ef724f9660f2c0105a8ed8d9f93e74ae06
Johan Roug <joro@sdc.dk>
2017-03-29 10:43:36 +0200

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,54 @@ const path = require('path');
 
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const extractSass = new ExtractTextPlugin({
-        filename: 'styles.[contenthash].css'
-      });
+  filename: 'styles.[contenthash].css'
+});
 
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const cleanConfig = new CleanWebpackPlugin(['build/*'], {
-      root: '',
-      verbose: true,
-      dry: false,
-      exclude: ['example.js']
-    })
+  root: '',
+  verbose: true,
+  dry: false,
+  exclude: ['example.js']
+})
 
 const config = {
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: [ '.ts', '.js' ]
-    },
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: ['babel-loader', 'ts-loader']
-            },
-            {
-                test: /\.scss$/,
-                use: extractSass.extract({
-                    use: [{
-                        loader: "css-loader", options: {
-                            sourceMap: true
-                        }
-                    }, {
-                        loader: "sass-loader", options: {
-                            sourceMap: true
-                        }
-                    }]
-                })
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: "css-loader", options: {
+              sourceMap: true
             }
-        ]
-    },
-    plugins: [
-        extractSass,
-        cleanConfig
+          }, {
+            loader: "sass-loader", options: {
+              sourceMap: true
+            }
+          }]
+        })
+      }
     ]
+  },
+  plugins: [
+    extractSass,
+    cleanConfig
+  ]
 };
 
 module.exports = config;
