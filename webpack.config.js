a2d3eb28aa370315b9156a03dcfd40b51ec1c86b
Johan Roug <joro@sdc.dk>
2017-03-29 13:28:42 +0200

    comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 const webpack = require('webpack');
 const path = require('path');
 
-const VENDOR_LIBS = [
-  "jquery","lodash" // this takes our vendor js files that we want in a seperate file
+//*************PLUGINS***************All called in bottom of file***************************************/
+// List of vendor JS libraries we want in a seperate vendor.js file
+const VENDOR_LIBS = [ // this takes our vendor js files that we want in a seperate file
+  "jquery",
+  "lodash"
 ];
 
+// Extract styles
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const extractSass = new ExtractTextPlugin({
   filename: 'styles.[contenthash].css'
 });
 
+// Clean our build folder
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const cleanConfig = new CleanWebpackPlugin(['build/*'], {
   root: '',
@@ -18,18 +23,21 @@ const cleanConfig = new CleanWebpackPlugin(['build/*'], {
   exclude: ['example.js']
 })
 
+// if we e.g. import jquery in our code, and also has it in our vendor.js file, remove them from our output bundle code, and only include it in vendor.js
 const optimize = new webpack.optimize.CommonsChunkPlugin({
-  name: 'vendor' // if we fx import jquery in our code, and also has then in our vendor.js file, remove them from our output code
+  name: 'vendor'
 });
 
+
+//*************WEBPACK CONFIG***************************************************************/
 const config = {
   entry: {
-    bundle: './src/index.ts', // output bundle.js
-    vendor: VENDOR_LIBS // output vendor.js
+    bundle: './src/index.ts', // Our whole codebase starts here. Our bundle will be called "bundle"
+    vendor: VENDOR_LIBS // Our vendors, and output file will be named "vendor"
   },
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: '[name].js'
+    filename: '[name].js' // output bundle.js and vendor.js
   },
   resolve: {
     extensions: ['.ts', '.js']
@@ -58,7 +66,7 @@ const config = {
       }
     ]
   },
-  plugins: [
+  plugins: [ // Our plugin from the top, are called here
     extractSass,
     cleanConfig,
     optimize
