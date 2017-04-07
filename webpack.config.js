19f4bc539e4bb88bb271e84c1b175417f8850f73
Johan Roug <joro@sdc.dk>
2017-04-07 13:37:53 +0200

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -242,7 +242,7 @@ module.exports = function makeWebpackConfig() {
 
       // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
       // Minify all javascript, switch loaders to minimizing mode
-      new webpack.optimize.UglifyJsPlugin({sourceMap: false, mangle: { keep_fnames: true }, compress: true}),
+      new webpack.optimize.UglifyJsPlugin({sourceMap: false, mangle: { keep_fnames: true }}),
 
       // Copy assets from the public folder
       // Reference: https://github.com/kevlened/copy-webpack-plugin
