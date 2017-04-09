471fd83c30340f70691512fb585a42e0b36722ef
DESKTOP-IQ2GSM8\Johan <johanroug@gmail.com>
2017-04-09 23:42:06 +0200

    no message

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,8 +1,12 @@
+import { Ingredient } from '../shared/ingredient.model';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient("Suckar", 2),
+    new Ingredient("Apples", 10)
+  ];
 }
