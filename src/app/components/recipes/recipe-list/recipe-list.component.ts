e523e5cc8939757329b6556a7b7ff3bfc6e25ed6
DESKTOP-IQ2GSM8\Johan <johanroug@gmail.com>
2017-04-09 22:45:30 +0200

    no message

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,11 @@
+import { Recipe } from '../recipe.model';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe("A test recipe", "This is simply a test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
+  ];
 }
