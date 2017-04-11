ebccc01a28d73b6dd5a954ab727287430dd20f4f
DESKTOP-IQ2GSM8\Johan <johanroug@gmail.com>
2017-04-11 10:53:48 +0200

    made navigation work with ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import '../styles/styles.scss';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
+  link: string = "recipes";
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() chosenLink = new EventEmitter<string>();
 
+  choseLink(link: string) {
+    this.chosenLink.emit(link);
+  }
 }
