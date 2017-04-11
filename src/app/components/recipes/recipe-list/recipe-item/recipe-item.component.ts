e9f8bfba49f5a16d729da56bc1d30928e4820538
DESKTOP-IQ2GSM8\Johan <johanroug@gmail.com>
2017-04-11 12:46:51 +0200

    no message

--- src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Recipe } from '../../recipe.model';
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html'
 })
 export class RecipeItemComponent {
-
+  @Input() recipe: Recipe;
 }
