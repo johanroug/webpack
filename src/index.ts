da43eaa65e38990458d2baf1c181500066953f4e
DESKTOP-IQ2GSM8\Johan <johanroug@gmail.com>
2017-04-17 23:08:09 +0200

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
 import * as $ from 'jquery';
-import {} from 'googlemaps'; // @typings for google maps
 import loadGoogleMapsAPI from 'load-google-maps-api'; // Simply takes below code and injects into scripttag on load
-
 import './styles/styles.scss';
 
-loadGoogleMapsAPI(
-  {
-    key: 'write key here',
-    language: 'da'
-  }).then((googleMaps) => {
-    let vanlose = {lat: 55.695142, lng: 12.498335};
-    let map = new googleMaps.Map(document.getElementById('map'), {
-      zoom: 17,
-      center: vanlose
-    });
-    new googleMaps.Marker({
-      position: vanlose,
-      map: map
-    });
-  }).catch((err) => {
+$(function() {
+  $('body').css('background', 'green');
+});
+
+loadGoogleMapsAPI({
+  key: 'write key here',
+  language: 'da'
+}).then((googleMaps) => {
+  let vanlose = { lat: 55.695142, lng: 12.498335 };
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: vanlose
+  });
+  new google.maps.Marker({
+    position: vanlose,
+    map: map
+  });
+}).catch((err) => {
   console.error(err);
 });
