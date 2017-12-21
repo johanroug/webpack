64cfd2f87aabd1f8dcdac7c3cbfefa27f4197088
Johan Roug <jro@addition.dk>
2017-12-21 15:38:30 +0100

    multi pointers

--- src/index.js
+++ src/index.js
@@ -2,18 +2,30 @@ import loadGoogleMapsAPI from 'load-google-maps-api'; // Simply takes below code
 import './styles/styles.scss';
 
 loadGoogleMapsAPI({
-  key: 'key',
+  key: 'AIzaSyBjvbaEopZYsBYCh6iWiKZYf_jSB8dAUFM',
   language: 'da'
 }).then(() => {
-  let vanlose = { lat: 55.695142, lng: 12.498335 };
-  let map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 20,
-    center: vanlose
+  const coordinates = [
+    {'lat': 55.695142, 'long': 12.498335},
+    {'lat': 55.695558, 'long': 12.500135}
+  ];
+
+  let vanlose;
+  let myMap = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: new google.maps.LatLng(coordinates[0].lat, coordinates[0].long),
   });
-  new google.maps.Marker({
-    position: vanlose,
-    map: map
-  });
+
+  for (let i = 0; i < coordinates.length; i ++) {
+    const coords = coordinates[i];
+    const latLng = new google.maps.LatLng(coords.lat,coords.long);
+    
+    new google.maps.Marker({
+      position: latLng,
+      map: myMap
+    });
+  }
+
 }).catch((err) => {
   console.error(err);
 });
